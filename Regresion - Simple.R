0fb7ee8e0995fa79ceea1e32fbe0c05d61427213
bonsoul <bonsoul24@gmail.com>
2025-02-15 18:24:36 +0300

    Regression

--- Regresion - Simple.R
+++ Regresion - Simple.R
@@ -6,3 +6,12 @@ model <- lm(mpg ~ wt, data = mtcars)
 
 #summary
 summary(model)
+
+
+#plotting
+png("Linear Regression.png")
+plot(mtcars$wt, mtcars$mpg,main="Linear Regression:: mpg vs wt",
+     xlab = "Weight (wt)", ylab = "Miles per Gallon (mpg")
+
+#add the regression line
+abline(model, col='red')
